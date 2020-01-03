68081d8392a359ce31b56f873b49426143f74a9d
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 15:08:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -93,7 +93,7 @@ def movies_with_directors_set(source)
     name2 = name[:name]
     name3 = name[:movies]
     
-    output << movies_with_director_key(name2, name3)
+    output << movies_with_director_key(source[index][:name], source[index][:movies])
     index += 1
   end
   return output
